de2b002477de480dcd1fb17d95ca11e5771645ac
Shestakov Artem <art.shestakov@icloud.com>
2023-12-19 12:40:23 +0300

    Добавлен файл проекта, задание и gitignore

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <filesystem>
+#include <thread>
+//-----------------------------------------------------------------------------
+int main(int argc, char* argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    return EXIT_SUCCESS;
+}
+//-----------------------------------------------------------------------------
